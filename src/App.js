f76a9ed43ddf7b2de0d1ce5cf1b1d6652c398877
gleb-code <57091508+gleb-code@users.noreply.github.com>
2020-09-10 17:54:25 +0300

    Revert "react-task1"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-import Card from './Card/Card';
-import Header from './Header/Header';
-
-
-class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="card">
-          <Card />
-        </div>
-      </div>
-    );
-  }
-    
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
